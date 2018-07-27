f21c081f70fe8cefb0d73bf55ad6e12d16b3a85e
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2018-07-27 14:01:17 -0400

    Hide site guides based on 'hidden' tag. Add intermediate pages to homepage

--- src/app/guides/guides.component.ts
+++ src/app/guides/guides.component.ts
@@ -37,23 +37,23 @@ export class GuidesComponent implements OnInit, OnDestroy {
 	}
 
 	filterGuides(filter = "") {
-		if (filter && filter.trim().length > 0) {
-			let keywords = filter.toLowerCase().split(/\s+/gi);
-			this.buffer = this.guides.filter((guide: any) => {
-				for (let k of keywords) {
-					if (!((guide.description && (guide.description + "").toLowerCase().indexOf(k) >= 0)
-						|| (guide.title && (guide.title + "").toLowerCase().indexOf(k) >= 0)
-						|| (guide.tags && (guide.tags + "").toLowerCase().indexOf(k) >= 0)
-						|| (guide.keywords && (guide.keywords + "").toLowerCase().indexOf(k) >= 0)
-					)) {
-						return false;
-					}
+		let keywords = filter.toLowerCase().split(/\s+/gi);
+		this.buffer = this.guides.filter((guide: any) => {
+			console.log(guide.title, guide.tags)
+			if (guide.tags && (guide.tags + "").toLowerCase().indexOf("hidden") >= 0) {
+				return false;
+			}
+			for (let k of keywords) {
+				if (!((guide.description && (guide.description + "").toLowerCase().indexOf(k) >= 0)
+					|| (guide.title && (guide.title + "").toLowerCase().indexOf(k) >= 0)
+					|| (guide.tags && (guide.tags + "").toLowerCase().indexOf(k) >= 0)
+					|| (guide.keywords && (guide.keywords + "").toLowerCase().indexOf(k) >= 0)
+				)) {
+					return false;
 				}
-				return true;
-			});
-		} else {
-			this.buffer = this.guides;
-		}
+			}
+			return true;
+		});
 	}
 
 }
